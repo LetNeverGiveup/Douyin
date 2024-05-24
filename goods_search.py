4c8ed91154e29eb16903290164732860f7c861bb
LetNeverGiveup <151518390+LetNeverGiveup@users.noreply.github.com>
2024-05-24 11:52:09 +0800

    Update goods_search.py
    增加排序

--- goods_search.py
+++ goods_search.py
@@ -1,11 +1,12 @@
 import requests
 
-get_url = 'http://124.223.166.207/dys/4aLRJG5U4cenNeVO/{}/{}'
+get_url = 'http://124.223.166.207/dys/4aLRJG5U4cenNeVO/{}/{}/{}'
 
 keywords = '夹克'     # 搜索商品关键词
 page = 1             # 第几页
+sort = 'price'       # 排序：price: 价格正序 sale: 销量倒序
 
-get_url = get_url.format(keywords, page)
+get_url = get_url.format(keywords, page, sort)
 
 response = requests.get(get_url)
 if response.status_code == 200:
